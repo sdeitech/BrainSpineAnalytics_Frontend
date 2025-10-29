8a7f8fc4eca19d6aad3b26c91a8b187699c45485
Tushar Chandel <tusharchandel@smartdatainc.net>
2025-10-29 14:28:09 +0530

    route changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,12 @@ import { AuthGuard } from './core/Auth-Gaurd/auth-guard.guard';
 export const routes: Routes = [
   //default
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  
-  //wild card if user enter wrong url 
-  { path: '**', redirectTo: 'login'},
-
+ 
   // normal
   { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent }  //,canActivate: [AuthGuard]
+  { path: 'dashboard', component: DashboardComponent },  //,canActivate: [AuthGuard]
+
+   //wild card if user enter wrong url 
+  { path: '**', redirectTo: 'login'},
+
 ];
