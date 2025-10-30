23158b79af2bdb72b48d658762a9623a196e10df
Tushar Chandel <tusharchandel@smartdatainc.net>
2025-10-30 18:22:03 +0530

    Server-ready-with-changes

--- src/app/core/layout/master-layout/master-layout.component.ts
+++ src/app/core/layout/master-layout/master-layout.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 import { LoginService } from '../../services/Login/login.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-master-layout',
   standalone: true,
-  imports: [CommonModule,
+  imports: [CommonModule,FormsModule,
     RouterLink,        
     RouterLinkActive, 
     RouterOutlet  ],
@@ -13,7 +14,9 @@ import { LoginService } from '../../services/Login/login.service';
   styleUrls: ['./master-layout.component.css']
 })
 export class MasterLayoutComponent implements OnInit {
+  Role:any=''
   isCollapsed = false;
+  selectedTab: string = 'operational';
   username = localStorage.getItem('username') || 'User';
   pageTitle = '';
   menuOpen = false;
@@ -23,11 +26,65 @@ export class MasterLayoutComponent implements OnInit {
     private loginService:LoginService
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     // Restore last theme (if saved)
+    this.Role = await this.loginService.getUserRole();
     this.darkTheme = localStorage.getItem('theme') === 'dark';
     document.body.classList.toggle('dark-theme', this.darkTheme);
   }
+
+    
+
+ menu = [
+    {
+      title: 'MAIN',
+      open: true,
+      toggle: true,
+      items: [
+        { label: 'Dashboard', link: '/dashboard' }   // Dashboard kept inside menu only
+      ]
+    },
+    {
+      title: 'PERFORMANCE',
+      toggle: true,
+      open: true,
+      items: [
+        { label: 'Revenue Analytics', link: '/revenueAnalytics' },
+        { label: 'Appointments', link: '/appointments' },
+        { label: 'Locations', link: '/locations' }
+      ]
+    },
+    {
+      title: 'CLINICAL OPERATIONS',
+      toggle: true,
+      open: true,
+      items: [
+        { label: 'Providers', link: '/providers' },
+        { label: 'TBI Insights', link: '/tbiInsights' },
+        { label: 'Diagnostics', link: '/diagnostics' },
+        { label: 'Telehealth', link: '/telehealth' }
+      ]
+    },
+    {
+      title: 'ADMIN',
+      toggle: true,
+      open: true,
+      items: [
+        { label: 'Staffing', link: '/staffing' },
+        { label: 'Referrals', link: '/referres' },
+        { label: 'Data Integration', link: '/dataIntergration' }
+      ]
+    },
+    {
+      title: 'Referrers',
+      toggle: true,
+      open: true,
+      items: [
+        { label: 'Referrals', link: '/referres' },
+      ]
+    }
+  ];
+
   isActive(url: string) {
     return this.router.url === url;
   }
@@ -52,17 +109,22 @@ export class MasterLayoutComponent implements OnInit {
   }
 
   toggleSidebar() {
-    this.isCollapsed = !this.isCollapsed;
-  }
-
-   toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+    //this.isCollapsed = !this.isCollapsed;
   }
 
   logout() {
     this.loginService.logout();
     this.router.navigate(['/login']);
   }
+  
 
+
+toggleMenu(m:any){
+  m.open = !m.open;
+}
+
+selectTab(tab: string) {
+  this.selectedTab = tab;
+}
  
 }
--- src/app/features/admin/data-integration/data-integration.component.ts
+++ src/app/features/admin/data-integration/data-integration.component.ts
@@ -1,12 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-integration',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    FormsModule],
   templateUrl: './data-integration.component.html',
   styleUrl: './data-integration.component.css'
 })
 export class DataIntegrationComponent {
+  fileType = 'CSV';
+  source = '';
+  uploadedFile: File | null = null;
 
+  onFileSelect(event: any) {
+    this.uploadedFile = event.target.files[0];
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    const file = event.dataTransfer?.files[0];
+    if (file) {
+      this.uploadedFile = file;
+    }
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  validateOnly() {
+    alert("✅ File validated successfully (mock)");
+  }
+
+  uploadProcess() {
+    if (!this.uploadedFile) {
+      alert("Please select file!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", this.uploadedFile);
+    formData.append("fileType", this.fileType);
+    formData.append("source", this.source);
+
+    alert(`✅ File Ready to upload: ${this.uploadedFile.name}`);
+    // API example:
+    // this.http.post('api/upload', formData).subscribe(...)
+  }
 }
